20ac89c5128c641c9971f6605a649a853adccc9d
Mohamed O. Haroun <mohamedhzfp@gmail.com>
2023-10-22 19:56:52 +0100

    Modified Main class

--- RestaurantApp/src/org/ulpgc/is1/control/Main.java
+++ RestaurantApp/src/org/ulpgc/is1/control/Main.java
@@ -1,16 +1,17 @@
 package org.ulpgc.is1.control;
 import org.ulpgc.is1.model.*;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static OrderManager init() {
-        OrderManager orderManager = new OrderManager();
+    public static OrderManager init(OrderManager orderManager) {
         orderManager.addCustomer("Ian", "Trujillo", "street",42, 34012,
                 "Las Palmas");
         orderManager.addCustomer("Mohamed", "O. Haroun", "street2", 43,24012, "Las palmas");
 
-        orderManager.addRestaurant("restaurant",new Phone("928737456"), new Menu("menu1", MenuType.Daily));
-        orderManager.addRestaurant("restaurant2",new Phone("928737455"), new Menu("menu2", MenuType.Tasting));
+        orderManager.addRestaurant("Restaurant 1",new Phone("928737456"), new Menu("menu1", MenuType.Daily));
+        orderManager.addRestaurant("Restaurant 2",new Phone("928737455"), new Menu("menu2", MenuType.Tasting));
 
         orderManager.addDish("dish1", "ingredient1,ingredient2", 10);
         orderManager.addDish("dish2", "ingredient2,ingredient3", 20);
@@ -20,15 +21,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        OrderManager orderManager = init();
+        OrderManager orderManager = new OrderManager();
+        init(orderManager);
         System.out.println(orderManager.getRestaurant(0));
         System.out.println(orderManager.getCustomer(1));
         System.out.println(orderManager.getDish(2));
         orderManager.order(0,0,new int[]{orderManager.getNumberDishes()- 1, orderManager.getNumberDishes()- 2}, new int[]{2,2});
 
         orderManager.removeCustomer(1);
-        System.out.println(orderManager.getNumberCustomer());
 
-        System.out.println(orderManager.getCustomer(0).getOrder(0).price());
+        System.out.print("Número de clientes: ");
+        System.out.println(orderManager.getNumberCustomers());
+
+        System.out.println(orderManager.getCustomer(0).getOrder(0));
     }
 }
